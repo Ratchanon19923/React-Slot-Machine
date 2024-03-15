106f46b9531c94092fa98f5d993ecb6c0ccf2e97
Ratchanon19923 <104318667+Ratchanon19923@users.noreply.github.com>
2024-03-15 15:22:19 +0700

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,32 @@
 import { useState, useEffect } from 'react';
 import '../css/style.css';
 
-
 function Home() {
-    const [position, setPosition] = useState([0, 0, 0]);
-    const [winner, setWinner] = useState(null);
+    const [position, setPosition] = useState([]);
+    const iconHeight = 188;
+    const multiplier = Math.floor(Math.random() * (4 - 1) + 1);
+    const speed = iconHeight * multiplier;
+    const [winner, setWinner] = useState(false);
     const [score, setScore] = useState(0);
 
+    const positions = [-940, -188, -0, -376, -1316, -1504, -564, -752, -1128];
 
+    const scores = [5, 10, 15, 20, 25, 30, 35, 40, 45];
+
+    useEffect(() => {
+        finishHandler();
+    }, []);
 
     useEffect(() => {
         if (position.length === 3) {
             const first = position[0];
             const results = position.every(match => match === first);
             setWinner(results);
+            if (results) {
+                const index = positions.findIndex(pos => pos === first);
+                const positionScore = scores[index];
+                setScore(prevScore => prevScore + positionScore);
+            }
         }
     }, [position]);
 
@@ -24,62 +37,30 @@ function Home() {
 
     const finishHandler = () => {
         const newPositions = Array.from({ length: 3 }, () => {
-            const multiplier = Math.floor(Math.random() * (4 - 1) + 1);
-            const iconHeight = 188;
-            const speed = iconHeight * multiplier;
-            const start = (Math.floor(Math.random() * 9) * iconHeight) * -1;
-            return start;
+            const randomIndex = Math.floor(Math.random() * positions.length);
+            return positions[randomIndex];
         });
         setPosition(newPositions);
-
-        if (position.length === 3) {
-            const one = position[0];
-            const two = position[1];
-            const three = position[2];
-            if (position.every(match => match === one)) {
-                setScore(score + 10);
-                setWinner(true);
-            }
-            if (position.every(match => match === two)) {
-                setScore(score + 10);
-                setWinner(true);
-            }
-            if (position.every(match => match === three)) {
-                setScore(score + 10);
-                setWinner(true);
-            }
-            if (position[0] === position[1]) {
-                setScore(score + 1);
-                setWinner(false);
-            }
-            if (position[0] === position[2]) {
-                setScore(score + 1);
-                setWinner(false);
-            }
-            if (position[1] === position[2]) {
-                setScore(score + 1);
-                setWinner(false);
-            }
-        }
     };
 
-    console.log("position", position);
-    console.log("score", score);
-    console.log("winner", winner);
-
     return (
-        <div>
-            <h1 style={{ color: 'white' }}>
-                {winner === null ? 'Waiting…' : winner === true ? '🤑 Win! 🤑' : 'Lost'}
-            </h1>
+        <>
             <div className={`spinner-container`}>
-                <div style={{ backgroundPosition: '0px ' + position[0] + 'px' }} className={`icons`} />
-                <div style={{ backgroundPosition: '0px ' + position[1] + 'px' }} className={`icons`} />
-                <div style={{ backgroundPosition: '0px ' + position[2] + 'px' }} className={`icons`} />
+                {position.map((pos, index) => (
+                    <div key={index} style={{ backgroundPosition: '0px ' + pos + 'px' }} className={`icons`} />
+                ))}
             </div>
-            <button aria-label='Play again.' onClick={handleClick}>spin</button>
-            <div className='money-t'>คะแนน: {score}</div>
-        </div>
+            <div>
+                <h1 style={{ color: winner ? 'green' : 'red' }}>
+                    {winner ? 'Winner!' : 'Loss'}
+                </h1>
+                <h2 style={{ color: 'white' }}>คะแนน: {score}</h2>
+                <button aria-label='Play again.' onClick={handleClick} className='bt-spin'>spin</button>
+            </div>
+
+
+
+        </>
     );
 }
 
