5d9ae9b2583087a1d145ee6dced1089d9731988f
Reviver <hero_kong@live.com>
2024-04-23 18:32:31 +0700

    ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,108 +3,124 @@ import "../css/style.css";
 import LoadingScreen from "./LoadingScreen";
 
 function Home() {
-    const [position, setPosition] = useState([]);
-    const [count, setCount] = useState(0);
-    const iconHeight = 188;
-    const multiplier = Math.floor(Math.random() * (4 - 1) + 1);
-    const speed = iconHeight * multiplier;
-    const [winner, setWinner] = useState(false);
-    const [score, setScore] = useState(0);
-    const positions = [-940, -188, -0, -376, -1316, -1504, -564, -752, -1128];
-    const scores = [5, 10, 15, 20, 25, 30, 35, 40, 45];
-    const [isLoading, setIsLoading] = useState(true);
-    const [progress, setProgress] = useState(0);
+  const [position, setPosition] = useState([]);
+  const [count, setCount] = useState(0);
+  const [winner, setWinner] = useState(false);
+  const [score, setScore] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+  const [showAd, setShowAd] = useState(false); // เริ่มต้นโชว์โฆษณาเป็น false
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            if (progress < 100) {
-                setProgress(progress + 1);
-            } else {
-                clearInterval(timer);
-                setIsLoading(false);
-            }
-        }, 20);
-        return () => clearInterval(timer);
-    }, [progress]);
+  const iconHeight = 188;
+  const multiplier = Math.floor(Math.random() * (4 - 1) + 1);
+  const speed = iconHeight * multiplier;
+  const positions = [-940, -188, -0, -376, -1316, -1504, -564, -752, -1128];
+  const scores = [5, 10, 15, 20, 25, 30, 35, 40, 45];
 
-    useEffect(() => {
-        if (!isLoading) {
-            finishHandler();
-        }
-    }, [isLoading]);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (progress < 100) {
+        setProgress(progress + 1);
+      } else {
+        clearInterval(timer);
+        setIsLoading(false);
+        setShowAd(true); // เมื่อโหลดเสร็จแล้วแสดงโฆษณา
+      }
+    }, 20);
+    return () => clearInterval(timer);
+  }, [progress]);
 
-    useEffect(() => {
-        if (position.length === 3) {
-            const first = position[0];
-            const results = position.every((match) => match === first);
-            setWinner(results);
-            if (results) {
-                const index = positions.findIndex((pos) => pos === first);
-                const positionScore = scores[index];
-                setScore((prevScore) => prevScore + positionScore);
-            }
-        }
-    }, [position]);
-
-    useEffect(() => {
-        if (position.length === 3) {
-            const first = position[0];
-            const results = position.every(match => match === first);
-            setWinner(results);
-            if (results) {
-                const index = positions.findIndex(pos => pos === first);
-                const positionScore = scores[index];
-                setScore(prevScore => prevScore + positionScore);
-            }
-        }
-        if (count > 0) {
-            if (score <= 100) {
-                handleClick();
-            }
-        }
-
-
-    }, [position]);
-
-    const handleClick = () => {
-        setPosition([]);
-        finishHandler();
-        setCount(count + 1);
-
-    };
-
-    const finishHandler = () => {
-        const newPositions = Array.from({ length: 3 }, () => {
-            const randomIndex = Math.floor(Math.random() * positions.length);
-            return positions[randomIndex];
-        });
-        setPosition(newPositions);
-    };
-
-    if (isLoading) {
-        return <LoadingScreen progress={progress} />;
+  useEffect(() => {
+    if (!isLoading && !showAd) {
+      finishHandler(); // เมื่อโหลดเสร็จแล้วและไม่แสดงโฆษณา ให้เริ่มเกมส์
     }
+  }, [isLoading, showAd]);
 
+  useEffect(() => {
+    if (position.length === 3) {
+      const first = position[0];
+      const results = position.every((match) => match === first);
+      setWinner(results);
+      if (results) {
+        const index = positions.findIndex((pos) => pos === first);
+        const positionScore = scores[index];
+        setScore((prevScore) => prevScore + positionScore);
+      }
+    }
+    if (count > 0 && score <= 100) {
+      handleClick();
+    }
+  }, [position, count]);
+
+  const handleClick = () => {
+    setPosition([]);
+    finishHandler();
+    setCount(count + 1);
+  };
+
+  const finishHandler = () => {
+    const newPositions = Array.from({ length: 3 }, () => {
+      const randomIndex = Math.floor(Math.random() * positions.length);
+      return positions[randomIndex];
+    });
+    setPosition(newPositions);
+  };
+
+  const handleAdClose = () => {
+    setShowAd(false);
+  };
+
+  if (isLoading) {
+    return <LoadingScreen progress={progress} />;
+  }
+
+  if (showAd) {
     return (
-        <>
-            <div className={`spinner-container`}>
-                {position.map((pos, index) => (
-                    <div key={index} style={{ backgroundPosition: '0px ' + pos + 'px' }} className={`icons`} />
-                ))}
-            </div>
-            <div>
-                <h2 style={{ color: 'white' }}>จำนวนครั้งที่กดได้: {count}</h2>
-                <h1 style={{ color: winner ? 'green' : 'red' }}>
-                    {winner ? 'Winner!' : 'Loss'}
-                </h1>
-                <h2 style={{ color: 'white' }}>คะแนน: {score}</h2>
-                <button aria-label='Play again.' onClick={handleClick} disabled={score >= 100} className='bt-spin'>spin</button>
-            </div>
-
-
-
-        </>
+      <div className="ad-container">
+        <div className="background-box">
+          <div className="sesion-box-top">
+            <img
+              src="../src/images/bnt.png"
+              alt="ปิด"
+              onClick={handleAdClose}
+              className="bnt-game"
+            />
+          </div>
+        </div>
+      </div>
     );
+  }
+
+  return (
+    <div className="background-box-">
+      <div className="session-box">
+        <div className="spinner-container">
+          {position.map((pos, index) => (
+            <div
+              key={index}
+              style={{ backgroundPosition: `0px ${pos}px` }}
+              className="icons"
+            />
+          ))}
+        </div>
+        <div>
+          <h2 style={{ color: "white" }}>จำนวนครั้งที่กดได้: {count}</h2>
+          <h1 style={{ color: winner ? "green" : "red" }}>
+            {winner ? "Winner!" : "Loss"}
+          </h1>
+          <h2 style={{ color: "white" }}>คะแนน: {score}</h2>
+          <button
+            aria-label="Play again."
+            onClick={handleClick}
+            disabled={score >= 100}
+            className="bt-spin"
+          >
+            spin
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -4,15 +4,21 @@ import Progressbar from "../components/Progressbar";
 function LoadingScreen({ progress }) {
   // รับค่า progress เข้ามาผ่าน props
   return (
-    <React.Fragment>
-      <div className="loading-screen">
-        <h1>Loading...</h1>
-        <div className="col-md-6">
-          <Progressbar value={progress} />{" "}
-          {/* ส่งค่า progress เข้าไปให้ Progressbar */}
+    <div className="background-box-load">
+      <div className="session-box">
+        <div className="sesion-box-top">
+          <React.Fragment>
+            <div className="loading-screen">
+              <img src="../src/images/load.png" className="logo-load" />
+              <div className="col-md-6">
+                <Progressbar value={progress} />{" "}
+                {/* ส่งค่า progress เข้าไปให้ Progressbar */}
+              </div>
+            </div>
+          </React.Fragment>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 }
 
